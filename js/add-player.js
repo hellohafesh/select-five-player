cec12d083142ece3d148cbf3c2bf16aec0da5774
soumik825 <allhafesh825@gmail.com>
2022-08-22 17:16:24 +0600

    Add Javascript Validation And Select players

--- js/add-player.js
+++ js/add-player.js
@@ -1,18 +1,71 @@
-function selectplayer() {
+function selectplayer(btnId, nameOfPayer) {
+    document.getElementById(btnId).addEventListener('click', function () {
 
-    const playerNameElement = document.getElementById('messi');
-    let playerName = playerNameElement.innerText;
+        const playerNameElement = document.getElementById(nameOfPayer);
+        let playerName = playerNameElement.innerText;
+        myFunction();
+        let list = document.getElementById('playerList');
 
 
-    var list = document.getElementById('playerList');
 
-    var firstName = playerName;
-    let player = document.createElement('li');
-    player.appendChild(document.createTextNode(firstName));
-    list.appendChild(player);
+        const playerListArray = document.getElementById('playerList');
+        const indexes = [];
+        for (let index = 0; index < playerListArray.length; index++) {
+            if (playerListArray[index] === 'a') {
+                indexes.push(index);
+            }
+        }
+
+        let arrayy = document.getElementById('playerList').children;
+
+        if (arrayy.length = 0, arrayy.length >= 5) {
+            let dBlock;
+            alert("Your Maximum Player Is 5!!");
+            return;
+
+        } else {
+            let firstName = playerName;
+            let player = document.createElement('li');
+            player.appendChild(document.createTextNode(firstName));
+            list.appendChild(player);
+        }
+
+        function myFunction() {
+            const btnCancle = document.getElementById('btnCancle');
+            if (btnCancle.style.display == 'none') {
+                btnCancle.style.display = 'block';
+            } else {
+                btnCancle.style.display = 'block';
+            }
+        }
+
+    })
+
 
 }
 
 
+function getcancle() {
+
+    document.getElementById('btnCancle').addEventListener('click', function () {
+        let listOfOl = document.getElementById('playerList');
+
+
+
+        let array = [];
+
+        let obj = listOfOl.innerText;
+
+        array.push(obj);
+        let poped = array.pop();
+
+        console.log(array);
+        console.log(poped);
+
+
+
+
+    })
+}
 
 
--- js/players.js
+++ js/players.js
@@ -1,65 +1,11 @@
 
 
-document.getElementById('btn-messi').addEventListener('click', function () {
-
-
-    const playerList = selectplayer();
-
-
-})
-document.getElementById('btn-messi').addEventListener('click', function () {
-
-
-    const playerList = selectplayer();
-
-
-})
-document.getElementById('btn-messi').addEventListener('click', function () {
-
-
-    const playerList = selectplayer();
-
-
-})
-document.getElementById('btn-messi').addEventListener('click', function () {
-
-
-    const playerList = selectplayer();
-
-
-})
-document.getElementById('btn-messi').addEventListener('click', function () {
-
-
-    const playerList = selectplayer();
-
-
-})
-document.getElementById('btn-messi').addEventListener('click', function () {
-
-
-    const playerList = selectplayer();
-
-
-})
-document.getElementById('btn-messi').addEventListener('click', function () {
-
-
-    const playerList = selectplayer();
-
-
-})
-document.getElementById('btn-messi').addEventListener('click', function () {
-
-
-    const playerList = selectplayer();
-
-
-})
-document.getElementById('btn-messi').addEventListener('click', function () {
-
-
-    const playerList = selectplayer();
-
-
-})
+const messi = selectplayer('btn-messi', 'messi');
+const bruyne = selectplayer('btn-bruyne', 'bruyne');
+const haaland = selectplayer('btn-haaland', 'haaland');
+const harry = selectplayer('btn-harry', 'harry');
+const karim = selectplayer('btn-karim', 'karim');
+const lewandowski = selectplayer('btn-lewandowski', 'lewandowski');
+const mbappe = selectplayer('btn-mbappe', 'mbappe');
+const ronaldo = selectplayer('btn-ronaldo', 'ronaldo');
+const salah = selectplayer('btn-salah', 'salah');
