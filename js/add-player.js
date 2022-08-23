268680e6ec913710b7a126fa87cd9ccec9fe4e02
soumik825 <allhafesh825@gmail.com>
2022-08-23 21:24:07 +0600

    do responsive clearly with display size

--- js/add-player.js
+++ js/add-player.js
@@ -5,11 +5,10 @@ function selectplayer(btnId, nameOfPayer) {
         let playerName = playerNameElement.innerText;
         btnFunction();
         // calculate();
+
         let list = document.getElementById('playerList');
-
-
-
         const playerListArray = document.getElementById('playerList');
+
         const indexes = [];
         for (let index = 1; index < playerListArray.length; index++) {
             if (playerListArray[index]) {
@@ -29,6 +28,7 @@ function selectplayer(btnId, nameOfPayer) {
             player.appendChild(document.createTextNode(firstName));
             list.appendChild(player);
         }
+
         //  button Cancle 
         function btnFunction() {
             const btnCancle = document.getElementById('btnCancle');
@@ -43,43 +43,7 @@ function selectplayer(btnId, nameOfPayer) {
         buttonElement.setAttribute('disabled', '');
 
 
-        document.getElementById('btnCalculate').addEventListener('click', function () {
-            const playerpaymentInput = getInputValueById('perPlayerInput');
-
-
-            if (isNaN(playerpaymentInput)) {
-                alert("Field Not Valid , Enter Money");
-                return;
-            }
-
-
-
-            const newpaymentTotal = playerpaymentInput * arrayy.length;
-            console.log(typeof (arrayy.length));
-            setTextElementById('playerExpenses', newpaymentTotal);
-        })
-
-
-        document.getElementById('calculateTotal').addEventListener('click', function () {
-            const playerExpenses = getElementValueById('playerExpenses');
-
-            const managerpaymentInput = getInputValueById('managerPay');
-            if (isNaN(managerpaymentInput)) {
-                alert("Field Not Valid , Enter Money");
-                return;
-            }
-
-            const coachpaymentInput = getInputValueById('coachPay');
-            if (isNaN(coachpaymentInput)) {
-                alert("Field Not Valid , Enter Money");
-                return;
-            }
-            const paymentTotal = playerExpenses + managerpaymentInput + coachpaymentInput;
-
-            setTextElementById('totalMoney', paymentTotal);
-        })
-
-
+        ssss(arrayy);
     })
 
 
--- js/utilitis.js
+++ js/utilitis.js
@@ -16,3 +16,33 @@ function setTextElementById(elementId, newValue) {
     element.innerText = newValue;
 }
 
+function ssss(arrayy) {
+    document.getElementById('btnCalculate').addEventListener('click', function () {
+        const playerpaymentInput = getInputValueById('perPlayerInput');
+
+        const newpaymentTotal = playerpaymentInput * arrayy.length;
+        error(playerpaymentInput);
+        setTextElementById('playerExpenses', newpaymentTotal);
+
+    })
+}
+
+function sssa() {
+    document.getElementById('calculateTotal').addEventListener('click', function () {
+        const playerExpenses = getElementValueById('playerExpenses');
+        const managerpaymentInput = getInputValueById('managerPay');
+        error(managerpaymentInput);
+        const coachpaymentInput = getInputValueById('coachPay');
+
+        const paymentTotal = playerExpenses + managerpaymentInput + coachpaymentInput;
+        setTextElementById('totalMoney', paymentTotal);
+    })
+}
+
+
+function error(playerpaymentInput) {
+    if (isNaN(playerpaymentInput)) {
+        alert("Field Not Valid , Enter Money");
+        return;
+    }
+} 
